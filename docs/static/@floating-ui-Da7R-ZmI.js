f12b4001a7da19810ff9a480e43cc21f0774ab2e
LiWeny16 <a454888395@gmail.com>
2024-06-19 04:25:46 +0800

    🎉update:Name of website

--- docs/static/@floating-ui-Da7R-ZmI.js
+++
@@ -1,1 +0,0 @@
-import"./react-BqHK-q4C.js";import"./react-dom-Df9FThwo.js";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,16 @@
-import "../style/index.css"
+import "../style/index.css" // 导入你的CSS文件
+
 import Home from "./home"
 import Chat from "../pages/chat"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/chat",
-    element: <Chat />,
-  },
-])
-export default function index() {
+import { BrowserRouter, Routes, Route } from "react-router-dom" // 导入正确的组件和函数
+
+export default function Index() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
